d42b025528d321aeeae08339ff074d3d455049ae
rohannagpure45 <nagpure.r@northeastern.edu>
2025-03-29 15:09:51 -0400

    chore: install @vitejs/plugin-react and fix dependencies

--- my-project/vite.config.ts
+++ my-project/vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Vite_React_Supabase/',
+  base: '/',
   plugins: [react()],
   build: {
     target: 'esnext',
